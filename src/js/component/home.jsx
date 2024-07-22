c175830d9cea778910f8be0f4666d238ef71c2a0
Beatriz Herranz <167246883+BeatrizHerranz@users.noreply.github.com>
2024-07-22 19:24:33 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+
+	const handleDelete = (index) => {
+		setTodos(todos.filter((_, currentIndex) => currentIndex !== index));
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>My Todos</h1>
+			<ul>
+				<li>
+					<input
+						type="text"
+						onChange={(e) => setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyPress={(e) => {
+							console.log("key was pressed", e.key);
+							if (e.key === "Enter") {
+								setTodos(todos.concat(inputValue));
+								setInputValue(""); // Limpiar el campo de entrada
+							}
+						}}
+						placeholder="What do you need to do?"></input>
+				</li>
+				{todos.length === 0 ? (
+					<li>No hay tareas, añadir tareas.</li>
+				) : (
+					todos.map((todo, index) => (
+						<li key={index}>
+							{todo}{" "}
+							<i
+								className="fa-solid fa-trash-can"
+								onClick={() => handleDelete(index)}
+							></i>
+						</li>
+					))
+				)}
+			</ul>
 		</div>
 	);
 };
